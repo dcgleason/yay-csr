cfcf635234a6023821c0482effc2cefe988e2ffc
Danny Gleason <danny.c.gleason@gmail.com>
2022-04-27 11:11:33 -0400

    change welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,15 @@ const App = () => {
   }, []);
 
   return (
-    <div className="App">
-  
+    <>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className='mt-10 h-20 w-100 font-medium mt underline leading-tight text-5xl mb-2 text-blue-700' >You & Yours</h1>
+      <h6 className= 'font-medium leading-tight text-base text-blue-700 d'>Gift meaningfully</h6>
+    </div> 
+    <div className="App justify-center w-full">
         <Input/>
-      
     </div>
+    </>
   );
 }
 
