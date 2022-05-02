31f6315765281e076a290eea006c2acd75a09422
Danny Gleason <danny.c.gleason@gmail.com>
2022-05-02 09:18:53 -0400

    fixed error

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -786,7 +786,7 @@ const options = {
                   />
                 </div>
                 <p className="mt-8 text-base text-gray-500">
-                 1. Prompts are sent email to your contributors to facilitate the message-writing process. A prompt may be, "What is your favorite quality of Eliza?" for example...if Eliza was recieving the gift. Contributors have two weeks to reply with their responses. 
+                 1. Prompts are sent via email to your contributors to facilitate the message-writing process. A prompt may be, "What is your favorite quality of Eliza?" for example...if Eliza was recieving the gift. Contributors have two weeks to reply with their responses. 
                 </p>
               </div>
               <div>
