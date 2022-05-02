e1308df339030151253107442de21d1eec798479
Danny Gleason <danny.c.gleason@gmail.com>
2022-05-02 13:31:11 -0400

    added money amount to alert message

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -141,7 +141,7 @@ const submitPayment = async () => {
       alert("Payment error: "+ paymentIntent.status.code + ", " + paymentIntent.status.decline_code);
     } else if (paymentIntent && paymentIntent.status === 'succeeded') {
       console.log("Your payment has succeeded", paymentIntent.status)
-      alert("Thank you. Your payment was successful. Emails will be sent to your gift contributor(s) shortly.");
+      alert("Thank you. Your payment of $20 was successful. Emails will be sent to your gift contributor(s) shortly.");
      
     }
   })();
