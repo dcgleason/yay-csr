cc4cf550a54d5a702c6483e86b0bf67a42721eaf
Danny Gleason <danny.c.gleason@gmail.com>
2022-05-02 13:55:39 -0400

    changed pay button

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -377,7 +377,7 @@ const submitRequest = async (e) => {
               className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 mt-6 w-full rounded focus:outline-none focus:shadow-outline"
               type="submit"
             >
-              Pay and Initate Gift
+              Submit Payment & Initate Gift
             </button>
           </div>
           </div>
