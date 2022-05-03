2ef1c7f6da09f04ca8c1ca0b024f03f82815408a
Danny Gleason <danny.c.gleason@gmail.com>
2022-05-03 11:43:30 -0400

    added required field message

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -291,7 +291,7 @@ const submitRequest = async (e) => {
               className="block text-gray-700 text-sm py-2 font-bold mb-2"
               htmlFor="Email"
             >
-              Your email address:
+              Your email address: (required)
             </label>
             <input
               className="shadow appearance-none border rounded w-full my-2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
