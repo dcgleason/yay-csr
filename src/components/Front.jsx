0e63a33cd69b7592feefc73dbf64881b41c1aec0
Danny Gleason <danny.c.gleason@gmail.com>
2022-05-04 14:30:03 -0400

    changed price

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -152,7 +152,7 @@ const userNavigation = [
 const product = {
   name: 'You & Yours. Share the love.',
   href: '#',
-  price: '$20 / book',
+  price: '$50 / book',
   description:
    '',
   imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-04-featured-product-shot.jpg',
