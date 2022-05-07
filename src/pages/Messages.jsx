ae4a8ea16e4a8318894729dae0ddea58b8a41f63
Danny Gleason <danny.c.gleason@gmail.com>
2022-05-07 19:16:54 -0400

    changed page title of messages page

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -10,7 +10,7 @@ const Messages = () => {
       <div className="space-y-8 divide-y divide-gray-200 sm:space-y-5">
         <div>
           <div>
-            <h3 className="text-lg mt-20 leading-6 font-medium text-gray-900">Message writing</h3>
+            <h3 className="text-lg mt-20 leading-6 font-medium text-gray-900">Words from you</h3>
             <p className="mt-1 max-w-2xl text-sm text-gray-500">
               Please answer the questions below about the gift recipient.
             </p>
