ed7cad7f5f2b045d0ae2084698722f06a48ad34a
Danny Gleason <danny.c.gleason@gmail.com>
2022-05-11 14:51:16 -0400

    added image comp

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import ImageUploading from 'react-images-uploading';
 
 
 const Messages = () => {
@@ -11,8 +12,26 @@ const Messages = () => {
   const [ additionalComments, setAdditionalComments ] = useState('');
   const [ contributorName, setContributorName ] = useState('');
   const [ giftCode, setGiftCode ] = useState('');
+  const [ file, setFile ] = useState('');
+
+  function handleUpload(event) {
+    setFile(event.target.files[0]);
+    console.log("file: " +file.name);
+
+    // Add code here to upload file to server
+    // ...
+  }
 
   const postMessagesMongoDB = async () => {
+
+    const formData = new FormData();
+    formData.append(
+      contributorName,
+      file,
+      file.name
+    );
+
+
     try{
       const resp =  await fetch("https://yay-api.herokuapp.com/insertMessageBundle", { 
         method: 'POST', 
@@ -60,7 +79,6 @@ const Messages = () => {
                   onChange={e => setContributorName(e.target.value)}
                   value={contributorName}
                   className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                  defaultValue={''}
                 />
                 </div>
               </div>
@@ -78,7 +96,6 @@ const Messages = () => {
                   onChange={e => setGiftCode(e.target.value)}
                   value={giftCode}
                   className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                  defaultValue={''}
                 />
                 </div>
               </div>
@@ -96,7 +113,6 @@ const Messages = () => {
                   onChange={e => setResponseOne(e.target.value)}
                   value={responseOne}
                   className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                  defaultValue={''}
                 />
                 <p className="mt-2 text-sm text-gray-500">Reponse 1</p>
               </div>
@@ -114,7 +130,6 @@ const Messages = () => {
                   onChange={e => setResponseTwo(e.target.value)}
                   value={responseTwo}
                   className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                  defaultValue={''}
                 />
                 <p className="mt-2 text-sm text-gray-500">Response 2</p>
               </div>
@@ -132,7 +147,6 @@ const Messages = () => {
                   onChange={e => setResponseThree(e.target.value)}
                   value={responseThree}
                   className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                  defaultValue={''}
                 />
                 <p className="mt-2 text-sm text-gray-500">Response 3</p>
               </div>
@@ -149,7 +163,6 @@ const Messages = () => {
                   onChange={e => setResponseFour(e.target.value)}
                   value={responseFour}
                   className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                  defaultValue={''}
                 />
                 <p className="mt-2 text-sm text-gray-500">Response 4</p>
               </div>
@@ -166,29 +179,29 @@ const Messages = () => {
                   onChange={e => setResponseFive(e.target.value)}
                   value={responseFive}
                   className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                  defaultValue={''}
                 />
                 <p className="mt-2 text-sm text-gray-500">Response 5</p>
               </div>
             </div>
 
 
-            <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-          <label for="cover-photo" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Photo: </label>
-          <div class="mt-1 sm:mt-0 sm:col-span-2">
-            <div class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-              <div class="space-y-1 text-center">
-                <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
-                  <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+
+            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+          <label htmlFor="cover-photo" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Photo: </label>
+          <div className="mt-1 sm:mt-0 sm:col-span-2">
+            <div className="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+              <div className="space-y-1 text-center">
+                <svg className="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                  <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" style={{ strokeWidth: 2, strokeLinecap:"round", strokeLinejoin:"round" }}/>
                 </svg>
-                <div class="flex text-sm text-gray-600">
-                  <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                    <span>Upload a file</span>
-                    <input id="file-upload" name="file-upload" type="file" class="sr-only"/>
+                <div className="flex text-sm text-gray-600">
+                  <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                    <span>Upload a image</span>
+                    <input id="file-upload" name="file-upload" type="file" onChange={handleUpload} className="sr-only"/>
                   </label>
-                  <p class="pl-1">or drag and drop</p>
+                  <p className="pl-1">here</p>
                 </div>
-                <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+                <p className="text-xs text-gray-500">PNG or JPG up to 16MB</p>
               </div>
             </div>
           </div>
@@ -207,7 +220,6 @@ const Messages = () => {
                   onChange={e => setAdditionalComments(e.target.value)}
                   value={additionalComments}
                   className="max-w-lg shadow-sm block w-full focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
-                  defaultValue={''}
                 />
                 <p className="mt-2 text-sm text-gray-500"></p>
               </div>
