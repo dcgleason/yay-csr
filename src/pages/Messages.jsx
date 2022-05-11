808a2f94c349c9bd9f0d201f10ead4352251e1fa
Danny Gleason <danny.c.gleason@gmail.com>
2022-05-11 10:17:54 -0400

    chaned photo label wording

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -190,7 +190,7 @@ const Messages = () => {
             </div>
 
             <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
-          <label for="cover-photo" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Photo </label>
+          <label for="cover-photo" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> A photo memory </label>
           <div class="mt-1 sm:mt-0 sm:col-span-2">
             <div class="max-w-lg flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
               <div class="space-y-1 text-center">
