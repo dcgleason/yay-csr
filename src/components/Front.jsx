50e4bc08c00eb69b199d6d5c6c5db49b5d04f052
Danny Gleason <danny.c.gleason@gmail.com>
2022-05-15 17:25:44 -0400

    added text

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -851,8 +851,8 @@ const options = {
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-24 w-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M3 19v-8.93a2 2 0 01.89-1.664l7-4.666a2 2 0 012.22 0l7 4.666A2 2 0 0121 10.07V19M3 19a2 2 0 002 2h14a2 2 0 002-2M3 19l6.75-4.5M21 19l-6.75-4.5M3 10l6.75 4.5M21 10l-6.75 4.5m0 0l-1.14.76a2 2 0 01-2.22 0l-1.14-.76" />
                   </svg>
-                  <h3 className="mt-6 text-base font-medium text-gray-900">Thank you</h3>
-                  <p className="mt-3 text-base text-gray-500">Send thank yous to a special someone.</p>
+                  <h3 className="mt-6 text-base font-medium text-gray-900">A Thank You</h3>
+                  <p className="mt-3 text-base text-gray-500">Send thank you's to a special someone.</p>
                 </div>
                 <div>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-24 w-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
