11645d91a59a0dfb00f8a61c19429b33a6fd4620
Danny Gleason <danny.c.gleason@gmail.com>
2022-05-15 17:28:42 -0400

    changed wording

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -838,14 +838,14 @@ const options = {
                   <path stroke-linecap="round" stroke-linejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                   </svg>
                   <h3 className="mt-6 text-base font-medium text-gray-900">Anniversaries</h3>
-                  <p className="mt-3 text-base text-gray-500">Wish someone a happy anniversary. Commemorate the couple.</p>
+                  <p className="mt-3 text-base text-gray-500">Wish someone a happy anniversary.</p>
                 </div>
                 <div>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-24 w-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
               </svg>
                   <h3 className="mt-6 text-base font-medium text-gray-900">Weddings</h3>
-                  <p className="mt-3 text-base text-gray-500">Give a couple a gift they can cherrish for years to come.</p>
+                  <p className="mt-3 text-base text-gray-500">Give a the bride (or groom) a gift they can cherrish for years to come.</p>
                 </div>
                 <div>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-24 w-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
