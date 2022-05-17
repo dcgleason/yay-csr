08cd50efe2a812f882b674fd7781bffaae253b1f
Danny Gleason <danny.c.gleason@gmail.com>
2022-05-17 10:10:52 -0400

    changed wording

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -841,10 +841,10 @@ const options = {
           <section aria-labelledby="details-heading">
             <div className="flex flex-col items-center text-center">
               <h2 id="details-heading" className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                How we do it:
+                How to create an Amore Book?
               </h2>
               <p className="mt-3 max-w-3xl text-lg text-gray-600">
-                (Hint: we make sure <em>you</em> get all the credit)
+                (We make sure <em>you</em> get all the credit)
               </p>
             </div>
 
@@ -858,7 +858,7 @@ const options = {
                   />
                 </div>
                 <p className="mt-8 text-base text-gray-500">
-                 1. After you initiate the gift-creation process, the family, friends, and/or coworkers of the gift recipient, i.e. your contributors, will be prompted to input heartfelt messages from the <a className="underline font-bold" href="https://amorebooks.io/write">Write</a> page about your gift recipient (we send them an email with a link to the the <a className="underline font-bold" href="https://amorebooks.io/write">Write</a> page). Contributors have 7 days to write and submit their responses. 
+                 1. After you initiate the gifting process, the family, friends, and/or coworkers of the gift recipient, i.e. your contributors, will be prompted to input heartfelt messages from the <a className="underline font-bold" href="https://amorebooks.io/write">Write</a> page about your gift recipient (we send them an email with a link to the the <a className="underline font-bold" href="https://amorebooks.io/write">Write</a> page). Contributors have 7 days to write and submit their responses. 
                 </p>
               </div>
               <div>
