ca704119e1d0effe5df1a18e610570c7dab29e2a
Danny Gleason <danny.c.gleason@gmail.com>
2022-07-07 15:13:30 -0400

    added time duration to sub text

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -787,7 +787,7 @@ const options = {
                 </div>
 
                 <div className="mt-4 space-y-6">
-                  <p className="text-base text-gray-500"><b>Bundle</b>,  <em>noun</em>: a book of written messages of love and support collected for a special person in your life from <em>his or her</em> family and friends. <br></br><br></br> We coordinate everything for you and give <em>you</em> all the credit. </p>
+                  <p className="text-base text-gray-500"><b>Bundle</b>,  <em>noun</em>: a book of written messages of love and support collected for a special person in your life from <em>his or her</em> family and friends. <br></br><br></br> We coordinate everything for you within 7 days and give <em>you</em> all the credit. </p>
                 </div>
                 <div className="mt-6 text-center">
                     <a href="#" className="group inline-flex text-base font-medium">
