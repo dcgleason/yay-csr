2ee6837e346d4eeef214dfc96479ef740588902d
Danny Gleason <danny.c.gleason@gmail.com>
2022-07-14 17:03:32 -0400

    added to question

--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -139,7 +139,7 @@ const Messages = () => {
 
             <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
               <label htmlFor="about" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
-                Describe how Sally impacted you positively. Are you grateful? In what ways? 
+                Describe how Sally impacted you positively. Are you grateful? If so, in what ways? 
               </label> 
               <div className="mt-1 sm:mt-0 sm:col-span-2">
                 <textarea
